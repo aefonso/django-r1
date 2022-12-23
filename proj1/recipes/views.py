cc39ca5ec2342fcce22cedada5c9d0a0df2ae599
aefonso <hafonso021@gmail.com>
2022-12-23 09:45:12 -0300

    feat(views): criando estrutura das páginas

--- proj1/recipes/views.py
+++ proj1/recipes/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# HTTP RESPONSE
+
+def home(request):
+    return HttpResponse('HOME - Hello Django ')
+
+def sobre(request):
+    return HttpResponse('SOBRE - Hello Django')
+
+def contato(request):
+    return HttpResponse('CONTATO - Hello Django')
