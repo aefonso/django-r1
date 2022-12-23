9f23dd936565b2f283ee533d49e23e7f37f3bf67
aefonso <hafonso021@gmail.com>
2022-12-23 09:46:33 -0300

    feat(urls): comentando um import

--- proj1/prjDjango1/urls.py
+++ proj1/prjDjango1/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 from recipes.views import home, sobre, contato
 
