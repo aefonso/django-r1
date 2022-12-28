9cf94f55eeb092540cd6741cc18a86065bfef633
aefonso <hafonso021@gmail.com>
2022-12-28 07:11:16 -0300

    feat(recipes): update

--- proj1/recipes/views.py
+++ proj1/recipes/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
+from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here
 # HTTP RESPONSE
 
 def home(request):
     return render(request, 'home.html')
 
 def sobre(request):
-    return HttpResponse('<h1>SOBRE - Hello Django</h1>')
+    return HttpResponse('SOBRE - Hello Django')
 
 def contato(request):
-    return HttpResponse('<h1>CONTATO - Hello Django</h1>')
+    return HttpResponse('CONTATO - Hello Django')
+# Create your views here.
